3f2d3d073417e758d27a925e7a0128f30e5a42c2
jj b <jj@thebabers.at>
2021-01-14 10:26:09 +0000

    added reformatting and config load

--- misc/__init__.py
+++ misc/__init__.py
@@ -1,6 +1,5 @@
 import os
 import appdirs
 
-from misc import *
+user_data_dir = str(os.path.join(appdirs.user_data_dir(), "MemriseBot"))
 
-user_data_dir = str(os.path.join(appdirs.user_data_dir(), "md5-unhasher"))
--- misc/config.py
+++ misc/config.py
@@ -1,3 +1,8 @@
+import os
+
 from kivy.config import ConfigParser
 
+from misc import user_data_dir
+
 Config = ConfigParser()
+Config.read(os.path.join(user_data_dir, "config.ini"))
