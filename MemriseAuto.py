2af445971fb0c27926389a2a5019d09e01672b24
jj b <jj@thebabers.at>
2021-01-14 10:43:15 +0000

    added setup and config file creation

--- MemriseAuto.py
+++ MemriseAuto.py
@@ -1,5 +1,33 @@
+import pathlib
+from multiprocessing import freeze_support
+import os
+
 if __name__ == '__main__':
+    freeze_support()
+
+    from misc import user_data_dir
+
+    if not os.path.exists(user_data_dir):
+        os.mkdir(user_data_dir)
+
+    if not os.path.exists(os.path.join(user_data_dir, "config.ini")):
+        from misc.config import create
+        create()
+
+    os.chdir(pathlib.Path(__file__).parent.absolute())
+    os.environ["KIVY_HOME"] = str(os.path.join(user_data_dir, "kivy"))
+    os.environ["KCFG_KIVY_LOG_NAME"] = "%y-%m-%d_%_.log"
+    os.environ["KCFG_KIVY_LOG_DIR"] = "../logs"
+    os.environ["KCFG_KIVY_LOG_LEVEL"] = "info"
+
+    import kivy
+    from kivy.logger import Logger
+
+    Logger.info("UserDataDir: UserDataDir at \"" + str(user_data_dir) + "\"")
+
+    from misc import *
     from Gui import *
+
 
     app = MemriseAutoApp()
     app.run()
--- misc/config.py
+++ misc/config.py
@@ -6,3 +6,11 @@ from misc import user_data_dir
 
 Config = ConfigParser()
 Config.read(os.path.join(user_data_dir, "config.ini"))
+
+
+def create():
+    for section in Config.sections():
+        Config.remove_section(section)
+
+    with open(os.path.join(user_data_dir, "config.ini"), 'w'):
+        Config.write()
