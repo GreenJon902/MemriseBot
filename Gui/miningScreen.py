fd0216a9add1cd2d42239a755f86d13eee4807cc
jj b <jj@thebabers.at>
2021-01-15 19:49:19 +0000

    sizing and added preview

--- Gui/miningScreen.py
+++ Gui/miningScreen.py
@@ -1,9 +1,16 @@
+import io
+import threading
+import time
 from threading import Thread
 
 from kivy import Logger
+from kivy.clock import Clock
 from kivy.graphics import Line
 from kivy.properties import StringProperty, BooleanProperty, NumericProperty, OptionProperty
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import Screen
+from kivy.core.image import Image as CoreImage
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
@@ -20,6 +27,8 @@ class MiningScreen(Screen):
     requireAll = BooleanProperty(True)
     mode = OptionProperty(Config.get("Mining", "mode"), options=["Blatant", "Ghost"])
     midLinePos = NumericProperty(0)
+
+    driver = None
 
     def on_pre_enter(self, *args):
         self.mode = Config.get("Mining", "mode")
@@ -43,9 +52,11 @@ class MiningScreen(Screen):
         self.ids["InfoLabelHidden"].texture_update()
 
         self.midLinePos = self.ids["InfoLabelHidden"].texture_size[0] + self.ids["InfoLabelHidden"].right
+        self.ids["LabelHolder"].width = self.midLinePos
 
     def on_size(self, *args, **kwargs):
         self.draw()
+
 
     def on_midLinePos(self, *args, **kwargs):
         self.draw()
@@ -60,16 +71,22 @@ class MiningScreen(Screen):
     def on_enter(self, *args):
         Thread(target=self.mine).start()
 
+    def update_image(self, _):
+        data = io.BytesIO(self.driver.get_screenshot_as_png())
+
+        coreImage = CoreImage(data, ext="png")
+        texture = coreImage.texture
+        self.ids["WebpageImage"].texture = texture
+
     def mine(self):
         Logger.info("Miner: Started mining function")
 
         chromedriver_autoinstaller.install()
-
         Logger.info("Miner: Chromedriver installed if not already")
 
         chrome_options = ChromeOptions()
         chrome_options.add_argument("--headless")
-
+        chrome_options.add_argument("--window-size=1920,1080")
         Logger.info("Miner: Chromedriver setup")
 
         url = Config.get("Mining", "url")
@@ -78,5 +95,13 @@ class MiningScreen(Screen):
 
         Logger.info("Miner: Loaded " + str(url))
 
+        self.driver = driver
+        screenShotClock = Clock.schedule_interval(self.update_image, 1)
+        Logger.info("Miner: Started window viewer clock")
 
 
+        time.sleep(100)
+
+
+        screenShotClock.cancel()
+        Logger.info("Miner: Finished mining function")
