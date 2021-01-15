74a20aa75463f155f3d67fa5478e0371ea032c03
jj b <jj@thebabers.at>
2021-01-15 13:22:50 +0000

    fix exit and rejoin

--- Gui/ConfigLayout.py
+++ Gui/ConfigLayout.py
@@ -23,6 +23,12 @@ class ConfigLayout(BoxLayout):
 
         self.bind(pos=self.draw_bg, size=self.draw_bg)
 
+    def on_exit(self):
+        for n, child in enumerate(reversed(self.children)):
+            child.opacity = 0
+
+        self.draw_bg()
+
     def draw_bg(self, *args):
         n = 0
         colors = (0.1, 0.1, 0.1), (0.15, 0.15, 0.15)
