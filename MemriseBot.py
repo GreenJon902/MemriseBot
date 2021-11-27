55e945c44b678cd7531e0720a1855a0e2b709634
GreenJon902 <GreenJon902@gmail.com>
2021-11-27 20:05:36 +0000

    added 8th step to totorial and added translation input box and start button

--- MemriseBot.py
+++ MemriseBot.py
@@ -6,6 +6,9 @@ class MemriseBot(App):
     def build(self):
         return Builder.load_file("resources/kv.kv")
 
+    def start_botting(self, translation):
+        print(f"Starting, translation is {translation}")
+
 
 if __name__ == "__main__":
     memriseBot = MemriseBot()
