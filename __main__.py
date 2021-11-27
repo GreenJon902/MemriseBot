d246b9e07ca0207efe4c0510fdd4fc3e42abd6b0
GreenJon902 <GreenJon902@gmail.com>
2021-11-27 11:48:01 +0000

    fixed some stuff

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ from tkinter import Tk, Label, Button, Text
 import time
 import selenium.webdriver
 import chromedriver_autoinstaller
+from selenium.webdriver.chrome.options import Options
 
 
 class id:
@@ -11,127 +12,78 @@ class id:
     def do(self):
         self.done = True
 
-
 chromedriver_autoinstaller.install()
-driver = selenium.webdriver.Chrome()
+
+options = Options()
+options.set_capability("unhandledPromptBehavior", "dismiss")
+
+driver = selenium.webdriver.Chrome(options=options)
 driver.get("https://www.memrise.com/home/")
 
-while True:
-    isDone = id()
 
-    window = Tk()
-    Label(window,
-          text="Place the window with memrise on next to this window. Go to the levels menu on the Course and \n" +
-               "then click on a level that has already been complected. Click the Ignore button, click All and \n" +
-               "then unselected one. Once this is done, scroll down to the bottom and click Save.").pack()
-    Button(window, text="Next", command=isDone.do).pack(),
+isDone = id()
 
-    while not isDone.done:
-        window.update()
+window = Tk()
+Label(window,
+      text="Place the window with memrise on next to this window. Go to the levels menu on the Course and \n" +
+           "then click on a level that has already been complected. Click the Ignore button, click All and \n" +
+           "then unselected one. Once this is done, scroll down to the bottom and click Save.").pack()
+Button(window, text="Next", command=isDone.do).pack(),
 
-    window.destroy()
+while not isDone.done:
+    window.update()
 
-    # -----------------------------------------------------------------------------------------------
-    isDone = id()
+window.destroy()
 
-    window = Tk()
-    Label(window,
-          text="Set the positions of the Review and the levels button by going through once, when you click \n" +
-               "change then you have 5 seconds to get the mouse over the button.").grid(row=0, column=0,
-                                                                                        columnspan=3)
-    Label(window, text=" ").grid(row=1, column=0, columnspan=3)
+# -----------------------------------------------------------------------------------------------
+isDone = id()
 
-    Label(window, text="Translation").grid(row=4, column=0)
-    t3 = Text(window, height=1)
-    t3.delete(1.0, "end")
-    t3.insert(1.0, "Not Set")
-    t3.grid(row=4, column=1)
+window = Tk()
+Label(window, text="Translation").grid(row=0, column=0)
+t3 = Text(window, height=1)
+t3.delete(1.0, "end")
+t3.insert(1.0, "Not Set")
+t3.grid(row=0, column=1)
 
-    Button(window, text="Next", command=isDone.do).grid(row=6, column=0, columnspan=3)
+Button(window, text="Next", command=isDone.do).grid(row=1, column=0, columnspan=2)
 
-    while not isDone.done:
-        window.update()
+while not isDone.done:
+    window.update()
 
-    trans = str(t3.get(1.0, "end-1c"))
+trans = str(t3.get(1.0, "end-1c"))
 
-    window.destroy()
+window.destroy()
 
-    # -----------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
 
-    isDone = id()
+isDone = id()
 
-    window = Tk()
-    Label(window, text="Running!").pack()
-    Button(window, text="Stop", command=isDone.do).pack()
+window = Tk()
+Label(window, text="Running!").pack()
+Button(window, text="Stop", command=isDone.do).pack()
 
 
-    for element in driver.find_elements_by_tag_name("a"):
-        if "Review" in element.text:
-            print(element.text)
-            path = element.get_attribute("href")
+for element in driver.find_elements_by_tag_name("a"):
+    if "Review" in element.text:
+        print(element.text)
+        path = element.get_attribute("href")
 
-            break
-
-    while not isDone.done:
-
-        window.update()
-
-        try:
-            driver.get(path)
-            time.sleep(1)
-            driver.find_element_by_class_name("typing-type-here").send_keys(trans)
-            time.sleep(0.1)
-            driver.find_element_by_class_name("next-icon").click()
-            time.sleep(0.1)
-            driver.find_element_by_class_name("next-icon").click()
-            time.sleep(0.1)
+        break
 
 
-        except:
-            print("failed")
-            time.sleep(0.5)
 
-            try:
-                driver.get(path)
-                time.sleep(1)
-                driver.find_element_by_class_name("typing-type-here").send_keys(trans)
-                time.sleep(0.1)
-                driver.find_element_by_class_name("next-icon").click()
-                time.sleep(0.1)
-                driver.find_element_by_class_name("next-icon").click()
-                time.sleep(0.1)
+while not isDone.done:
 
+    window.update()
 
-            except:
-                print("failed")
-                time.sleep(0.5)
-
-                try:
-                    driver.get(path)
-                    time.sleep(1)
-                    driver.find_element_by_class_name("typing-type-here").send_keys(trans)
-                    time.sleep(0.1)
-                    driver.find_element_by_class_name("next-icon").click()
-                    time.sleep(0.1)
-                    driver.find_element_by_class_name("next-icon").click()
-                    time.sleep(0.1)
+    try:
+        driver.get(path)
+        #time.sleep(1)
+        driver.find_element_by_class_name("typing-type-here").send_keys(trans)
+        #time.sleep(0.1)
+        driver.find_element_by_class_name("next-icon").click()
+        print("Worked")
 
 
-                except:
-                    print("failed")
-                    time.sleep(0.5)
-
-                try:
-                    driver.get(path)
-                    time.sleep(1)
-                    driver.find_element_by_class_name("typing-type-here").send_keys(trans)
-                    time.sleep(0.1)
-                    driver.find_element_by_class_name("next-icon").click()
-                    time.sleep(0.1)
-                    driver.find_element_by_class_name("next-icon").click()
-                    time.sleep(0.1)
-
-
-                except:
-                    print("ooooooooofffffff")
-                    time.sleep(0.5)
+    except Exception as e:
+        print("failed", e)
